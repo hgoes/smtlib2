1395bf2cac39fbba4bf8e1e7f3a7e2e405df9304
Henning Günther <guenther@forsyte.at>
2013-02-18 14:45:30 +0100

    Add Args instance for 6-tuple

--- Language/SMTLib2/Internals/Instances.hs
+++ Language/SMTLib2/Internals/Instances.hs
@@ -478,6 +478,30 @@ instance (LiftArgs a,LiftArgs b,LiftArgs c,LiftArgs d,LiftArgs e) => LiftArgs (a
     r5 <- unliftArgs x5
     return (r1,r2,r3,r4,r5)
 
+instance (Args a,Args b,Args c,Args d,Args e,Args f) => Args (a,b,c,d,e,f) where
+  type ArgAnnotation (a,b,c,d,e,f) = (ArgAnnotation a,ArgAnnotation b,ArgAnnotation c,ArgAnnotation d,ArgAnnotation e,ArgAnnotation f)
+  foldExprs f s ~(e1,e2,e3,e4,e5,e6) ~(ann1,ann2,ann3,ann4,ann5,ann6)
+    = let ~(s1,e1') = foldExprs f s e1 ann1
+          ~(s2,e2') = foldExprs f s1 e2 ann2
+          ~(s3,e3') = foldExprs f s2 e3 ann3
+          ~(s4,e4') = foldExprs f s3 e4 ann4
+          ~(s5,e5') = foldExprs f s4 e5 ann5
+          ~(s6,e6') = foldExprs f s5 e6 ann6
+      in (s6,(e1',e2',e3',e4',e5',e6'))
+
+instance (LiftArgs a,LiftArgs b,LiftArgs c,LiftArgs d,LiftArgs e,LiftArgs f) => LiftArgs (a,b,c,d,e,f) where
+  type Unpacked (a,b,c,d,e,f) = (Unpacked a,Unpacked b,Unpacked c,Unpacked d,Unpacked e,Unpacked f)
+  liftArgs (x1,x2,x3,x4,x5,x6) ~(a1,a2,a3,a4,a5,a6)
+    = (liftArgs x1 a1,liftArgs x2 a2,liftArgs x3 a3,liftArgs x4 a4,liftArgs x5 a5,liftArgs x6 a6)
+  unliftArgs (x1,x2,x3,x4,x5,x6) = do
+    r1 <- unliftArgs x1
+    r2 <- unliftArgs x2
+    r3 <- unliftArgs x3
+    r4 <- unliftArgs x4
+    r5 <- unliftArgs x5
+    r6 <- unliftArgs x6
+    return (r1,r2,r3,r4,r5,r6)
+
 instance Args a => Args [a] where
   type ArgAnnotation [a] = [ArgAnnotation a]
   foldExprs _ s _ [] = (s,[])
