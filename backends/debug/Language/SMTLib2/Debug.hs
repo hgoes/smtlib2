5ca999b52d87e4d59024036f7517b218689e7e69
Henning Günther <guenther@forsyte.at>
2014-05-01 10:02:35 +0200

    Add line numbers to debug output

--- backends/debug/Language/SMTLib2/Debug.hs
+++ backends/debug/Language/SMTLib2/Debug.hs
@@ -7,16 +7,30 @@ import Language.SMTLib2.Pipe
 import System.Console.ANSI
 import System.IO
 import Control.Monad.Trans
+import Data.IORef
 
-debugBackend :: b -> DebugBackend b
-debugBackend b = DebugBackend b stderr
+debugBackend :: b -> IO (DebugBackend b)
+debugBackend b = do
+  ref <- newIORef 0
+  return $ DebugBackend b stderr (Just ref)
 
 data DebugBackend b = DebugBackend { debugBackend' :: b
                                    , debugHandle :: Handle
+                                   , debugLines :: Maybe (IORef Integer)
                                    }
 
 instance (SMTBackend b m,MonadIO m) => SMTBackend (DebugBackend b) m where
   smtHandle b st req = do
+    case debugLines b of
+      Nothing -> return ()
+      Just line_ref -> do
+        cur_line <- liftIO $ atomicModifyIORef line_ref
+                    (\cl -> (cl+1,cl))
+        liftIO $ hSetSGR (debugHandle b) [Reset,SetColor Foreground Dull Red]
+        let line_str = show cur_line
+            line_str_len = length line_str
+            line_str' = replicate (4-line_str_len) ' '++line_str++" "
+        liftIO $ hPutStr (debugHandle b) line_str'
     case renderSMTRequest st req of
       Left l -> do
         liftIO $ hSetSGR (debugHandle b) [Reset,SetColor Foreground Dull Green]
