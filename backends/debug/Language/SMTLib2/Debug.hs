e950975c6288e70c283793cd78012d21b56f3257
Henning Günther <guenther@forsyte.at>
2014-11-24 15:06:31 +0100

    Add option to enable/disable color in debug backend

--- backends/debug/Language/SMTLib2/Debug.hs
+++ backends/debug/Language/SMTLib2/Debug.hs
@@ -7,22 +7,24 @@ import Language.SMTLib2.Pipe
 import System.Console.ANSI
 import System.IO
 import Control.Monad.Trans
+import Control.Monad (when)
 import Data.IORef
 
 debugBackend :: b -> IO (DebugBackend b)
 debugBackend b = do
   ref <- newIORef 0
-  return $ DebugBackend b stderr (Just ref) Nothing
+  return $ DebugBackend b stderr (Just ref) Nothing True
 
 namedDebugBackend :: String -> b -> IO (DebugBackend b)
 namedDebugBackend name b = do 
   ref <- newIORef 0
-  return $ DebugBackend b stderr (Just ref) (Just name)
+  return $ DebugBackend b stderr (Just ref) (Just name) True
 
 data DebugBackend b = DebugBackend { debugBackend' :: b
                                    , debugHandle :: Handle
                                    , debugLines :: Maybe (IORef Integer)
                                    , debugPrefix :: Maybe String
+                                   , debugUseColor :: Bool
                                    }
 
 instance (SMTBackend b m,MonadIO m) => SMTBackend (DebugBackend b) m where
@@ -30,30 +32,30 @@ instance (SMTBackend b m,MonadIO m) => SMTBackend (DebugBackend b) m where
     case debugPrefix b of
       Nothing -> return ()
       Just prf -> do
-        liftIO $ hSetSGR (debugHandle b) [Reset,SetColor Foreground Dull Cyan]
+        when (debugUseColor b) $ liftIO $ hSetSGR (debugHandle b) [Reset,SetColor Foreground Dull Cyan]
         liftIO $ hPutStr (debugHandle b) prf
     case debugLines b of
       Nothing -> return ()
       Just line_ref -> do
         cur_line <- liftIO $ atomicModifyIORef line_ref
                     (\cl -> (cl+1,cl))
-        liftIO $ hSetSGR (debugHandle b) [Reset,SetColor Foreground Dull Red]
+        when (debugUseColor b) $ liftIO $ hSetSGR (debugHandle b) [Reset,SetColor Foreground Dull Red]
         let line_str = show cur_line
             line_str_len = length line_str
             line_str' = replicate (4-line_str_len) ' '++line_str++" "
         liftIO $ hPutStr (debugHandle b) line_str'
     case renderSMTRequest st req of
       Left l -> do
-        liftIO $ hSetSGR (debugHandle b) [Reset,SetColor Foreground Dull Green]
+        when (debugUseColor b) $ liftIO $ hSetSGR (debugHandle b) [Reset,SetColor Foreground Dull Green]
         liftIO $ hPutStrLn (debugHandle b) (show l)
       Right msg -> do
-        liftIO $ hSetSGR (debugHandle b) [Reset,SetColor Foreground Dull White]
+        when (debugUseColor b) $ liftIO $ hSetSGR (debugHandle b) [Reset,SetColor Foreground Dull White]
         liftIO $ hPutStr (debugHandle b) $ unlines $ fmap (\xs -> ';':xs) (lines msg)
     resp <- smtHandle (debugBackend' b) st req
     case renderSMTResponse st req resp of
       Nothing -> return ()
       Just str -> do
-        liftIO $ hSetSGR (debugHandle b) [Reset,SetColor Foreground Dull Blue]
+        when (debugUseColor b) $ liftIO $ hSetSGR (debugHandle b) [Reset,SetColor Foreground Dull Blue]
         liftIO $ hPutStrLn (debugHandle b) str
-    liftIO $ hSetSGR (debugHandle b) [Reset]
+    when (debugUseColor b) $ liftIO $ hSetSGR (debugHandle b) [Reset]
     return resp
